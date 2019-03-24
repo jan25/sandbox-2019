735a3d5a2cfce82ef93323220b53931a8a1fde74
Abhilash Reddy <abhilash.reddy@booking.com>
2019-03-24 19:19:22 +0100

    helmify flask apps

--- basic-kubernetes/vagrant-synced/containers/backend-app/client/fibonacci.py
+++ basic-kubernetes/vagrant-synced/containers/backend-app/client/fibonacci.py
@@ -1,9 +1,11 @@
 import requests
+import os
 
-FIBONACCI_SVC_URL = 'http://fibonacci-service/fibonacci?n=%d'
+FIBONACCI_SVC_NAME = os.environ.get('FIBONACCI_SVC_NAME')
+FIBONACCI_HOST = 'http://%s' % FIBONACCI_SVC_NAME
 
 def get_nth(n):
     try:
-        return str(requests.get(FIBONACCI_SVC_URL % n).content)
+        return str(requests.get((FIBONACCI_HOST + '/fibonacci?n=%d') % n).content)
     except Exception as e:
         raise Exception(str(e))
